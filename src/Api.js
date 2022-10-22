bdf09826f52fb8ab4cafa72863dd63201ca1df53
Claudio Simões <91473739+claudiiosimoes@users.noreply.github.com>
2022-10-22 00:00:20 -0300

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -5,7 +5,7 @@ function Api() {
     const [variable, setVariable] = useState("Antes de chamar a API");
 
     const getApiLocal = async () => {
-        await fetch("http://127.0.0.1:5000/v1/aula/consultar", { // Habilitei o CORS no backend e funcionou!
+        await fetch("http://127.0.0.1:5000/v1/aula/consultar", {
             method: 'GET',
         })
             .then((res) => {
